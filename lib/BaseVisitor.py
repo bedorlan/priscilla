3b11c09173e2d07784837095c14266372081e3d1
bedorlan <orlando.bedoya@openintl.com>
2017-11-22 19:10:42 -0500

    explicitly reference the class variables in code

---
+++ lib/BaseVisitor.py
@@ -0,0 +1,213 @@
+import sys
+import ast
+import pdb
+from collections import deque
+
+sys.path.append('./built')
+from PlSqlLexer import PlSqlLexer
+from PlSqlParser import PlSqlParser
+from PlSqlParserVisitor import PlSqlParserVisitor
+
+OPERATORS = {
+    "=": ast.Eq,
+    "!=": ast.NotEq,
+    "<>": ast.NotEq,
+    ">": ast.Gt,
+    ">=": ast.GtE,
+    "<": ast.Lt,
+    "<=": ast.LtE,
+    "+": ast.Add,
+    "-": ast.Sub,
+    "*": ast.Mult,
+    "/": ast.Div
+}
+
+class BaseVisitor(PlSqlParserVisitor):
+# pylint: disable=I0011,C0103
+
+    def __init__(self):
+        self.pkgs_calls_found = []
+        self.vars_declared = []
+        self.pkg_name: str = None
+
+    def aggregateResult(self, aggregate, nextResult):
+        if aggregate is None:
+            aggregate = []
+        if nextResult is None:
+            return aggregate
+        aggregate.append(nextResult)
+        return aggregate
+
+    def visitParameter(self, ctx: PlSqlParser.ParameterContext):
+        ret = self.visitChildren(ctx)
+        name, *_ = full_flat_arr(ret)
+        return ast.arg(
+            arg=name,
+            annotation=None
+        )
+
+    def visitSeq_of_statements(self, ctx: PlSqlParser.Seq_of_statementsContext):
+        ret = self.visitChildren(ctx)
+        return ret
+
+    def visitStatement(self, ctx: PlSqlParser.StatementContext):
+        statements = self.visitChildren(ctx)
+        statement = statements[0]
+
+        return ast.Expr(
+            value=statement
+        )
+
+    def visitAssignment_statement(self, ctx: PlSqlParser.Assignment_statementContext):
+        ret = self.visitChildren(ctx)
+        ret = full_flat_arr(ret)
+        name, value, *_ = ret + [None, None]
+        return ast.Assign(
+            targets=[name],
+            value=value
+        )
+
+    def visitIf_statement(self, ctx: PlSqlParser.If_statementContext):
+        ret = self.visitChildren(ctx)
+        test = flat_arr(ret[0])[0]
+        body_expressions = flat_arr(ret[1])
+        return ast.If(
+            test=test,
+            body=body_expressions,
+            orelse=[]
+        )
+
+    def visitFunction_call(self, ctx: PlSqlParser.Function_callContext):
+        ret = self.visitChildren(ctx)
+        ret = full_flat_arr(ret)
+        ret = deque(ret)
+        routine_name = ret.popleft()
+        args = ret
+        return ast.Call(
+            func=routine_name,
+            args=args,
+            keywords=[]
+        )
+
+    def visitRoutine_name(self, ctx: PlSqlParser.Routine_nameContext):
+        ret = self.visitChildren(ctx)
+        pkg, method = flat_arr(ret)
+        if not pkg.id in self.pkgs_calls_found:
+            self.pkgs_calls_found.append(pkg.id)
+        return ast.Attribute(
+            value=pkg,
+            attr=method
+        )
+
+    def visitSeq_of_declare_specs(self, ctx:PlSqlParser.Seq_of_declare_specsContext):
+        ret = self.visitChildren(ctx)
+        ret = full_flat_arr(ret)
+        self.vars_declared = [assign.targets[0].id for assign in ret]
+        return ret
+
+    def visitVariable_declaration(self, ctx:PlSqlParser.Variable_declarationContext):
+        ret = self.visitChildren(ctx)
+        ret = full_flat_arr(ret)
+        ret = deque(ret)
+        name: ast.Name = ret.popleft()
+        value = ret[0] if len(ret) == 1 else ast.NameConstant(value=None)
+        return ast.Assign(
+            targets=[name],
+            value=value
+        )
+
+    def visitConcatenation(self, ctx: PlSqlParser.ConcatenationContext):
+        ret = self.visitChildren(ctx)
+        operands = full_flat_arr(ret)
+        if len(operands) == 2:
+            left, right = operands
+            operator = OPERATORS[ctx.op.text]()
+            return ast.BinOp(
+                left=left,
+                op=operator,
+                right=right
+            )
+        elif len(operands) == 1:
+            return operands
+        return ret
+
+    def visitRelational_expression(self, ctx: PlSqlParser.Relational_expressionContext):
+        ret = self.visitChildren(ctx)
+        expr = flat_arr(ret)
+        if len(expr) == 3:
+            left, operator, right = expr
+            return ast.Compare(
+                left=left,
+                ops=[operator],
+                comparators=[right]
+            )
+        return expr[0]
+
+    def visitType_spec(self, ctx:PlSqlParser.Type_specContext):
+        #return self.visitChildren(ctx)
+        #TODO
+        return None
+
+    def visitRelational_operator(self, ctx: PlSqlParser.Relational_operatorContext):
+        text = ctx.getText()
+        operator = OPERATORS[text]
+        return operator()
+
+    def visitGeneral_element(self, ctx:PlSqlParser.General_elementContext):
+        ret = self.visitChildren(ctx)
+        ret = full_flat_arr(ret)
+        name = ret[0]
+        if len(ret) == 1 and name.id in self.vars_declared:
+            return ret
+        ret = deque(ret)
+        # if the variable is not in the declare block, should be a class variable
+        if len(ret) == 1:
+            ret.appendleft(self.pkg_name)
+        value, attr = ret
+        attr = attr.id
+        return ast.Attribute(
+            value=value,
+            attr=attr
+        )
+
+    def visitRegular_id(self, ctx: PlSqlParser.Regular_idContext):
+        the_id = ctx.REGULAR_ID().getText().upper()
+        return ast.Name(id=the_id)
+
+    def visitNull_statement(self, ctx: PlSqlParser.Null_statementContext):
+        return ast.Pass()
+
+    def visitNumeric(self, ctx: PlSqlParser.NumericContext):
+        num = int(ctx.getText())
+        return ast.Num(n=num)
+
+    def visitQuoted_string(self, ctx: PlSqlParser.Quoted_stringContext):
+        str_value = ctx.CHAR_STRING().getText()[1:-1]
+        return ast.Str(str_value)
+
+def full_flat_arr(arr):
+    return [elem for elem in find_elems(arr)]
+
+def find_elems(arr):
+    if arr is None:
+        return
+    if not isinstance(arr, list):
+        yield arr
+        return
+    for elem in arr:
+        yield from find_elems(elem)
+
+def flat_arr(arr):
+    return [find_life(elem) for elem in arr]
+
+def find_life(arr):
+    if isinstance(arr, list):
+        if len(arr) > 0:
+            return find_life(arr[0])
+        else:
+            return None
+    else:
+        return arr
+
+def remove_duplicates(a_list: list) -> list:
+    return list(set(a_list))
--- lib/S2S.py
+++ lib/S2S.py
@@ -3,6 +3,7 @@ import ast
 from abc import ABC as Abstract, abstractmethod
 from collections import deque
 from pprint import pprint
+from typing import List
 import antlr4
 import astor
 import pdb
@@ -11,35 +12,16 @@ sys.path.append('./built')
 from PlSqlLexer import PlSqlLexer
 from PlSqlParser import PlSqlParser
 from PlSqlParserVisitor import PlSqlParserVisitor
+from BaseVisitor import *
+from Procedure_bodyVisitor import Procedure_bodyVisitor
 
-OPERATORS = {
-    "=": ast.Eq,
-    "!=": ast.NotEq,
-    "<>": ast.NotEq,
-    ">": ast.Gt,
-    ">=": ast.GtE,
-    "<": ast.Lt,
-    "<=": ast.LtE,
-    "+": ast.Add,
-    "-": ast.Sub,
-    "*": ast.Mult,
-    "/": ast.Div
-}
-
-class TheVisitor(PlSqlParserVisitor):
+class TheVisitor(BaseVisitor):
 # pylint: disable=I0011,C0103
 
     def __init__(self):
-        self.pkgs_calls_found = []
-        self.pkgs_in_file = []
-
-    def aggregateResult(self, aggregate, nextResult):
-        if aggregate is None:
-            aggregate = []
-        if nextResult is None:
-            return aggregate
-        aggregate.append(nextResult)
-        return aggregate
+        BaseVisitor.__init__(self)
+        self.pkgs_in_file: List[str] = []
+        self.pkg_name: str = None
 
     def create_imports(self):
         imports = []
@@ -68,191 +50,49 @@ class TheVisitor(PlSqlParserVisitor):
         return flat
 
     def visitCreate_package(self, ctx: PlSqlParser.Create_packageContext):
-        # TODO: for now ignoring
-        return None
-
-    def visitCreate_package_body(self, ctx: PlSqlParser.Create_package_bodyContext):
         ret = self.visitChildren(ctx)
         ret = full_flat_arr(ret)
         ret = deque(ret)
-        name: ast.Name = ret.popleft()
+        name: str = ret.popleft().id
+        name = get_spec_classname_by_classname(name)
         body = ret
-        self.pkgs_in_file.append(name.id)
+        if len(body) == 0:
+            body.append(ast.Pass())
         return ast.ClassDef(
-            name=name.id,
+            name=name,
             body=body,
             decorator_list=[],
             bases=[]
         )
 
-    def visitProcedure_body(self, ctx: PlSqlParser.Procedure_bodyContext):
+    def visitCreate_package_body(self, ctx: PlSqlParser.Create_package_bodyContext):
+        self.pkg_name = name = ctx.package_name()[0].getText().upper()
         ret = self.visitChildren(ctx)
         ret = full_flat_arr(ret)
-        ret = deque(ret)
-        name = ret.popleft()
-        args = []
-        while True:
-            arg = ret[0]
-            if not isinstance(arg, ast.arg):
-                break
-            args.append(arg)
-            ret.popleft()
-        body = ret
-        args = ast.arguments(
-            args=args,
-            defaults=[],
-            vararg=None,
-            kwarg=None
-        )
-        return ast.FunctionDef(
-            name=name.id,
-            args=args,
+        spec_classname = get_spec_classname_by_classname(name)
+        body = ret[1:]
+        self.pkgs_in_file.append(name)
+        return ast.ClassDef(
+            name=name,
             body=body,
-            decorator_list=[ast.Name(id="staticmethod")],
-            returns=None
+            decorator_list=[],
+            bases=[ast.Name(id=spec_classname)]
         )
 
-    def visitParameter(self, ctx: PlSqlParser.ParameterContext):
-        ret = self.visitChildren(ctx)
-        name, *_ = full_flat_arr(ret)
-        return ast.arg(
-            arg=name,
-            annotation=None
-        )
+    def visitProcedure_spec(self, ctx:PlSqlParser.Procedure_specContext):
+        #return self.visitChildren(ctx)
+        #TODO
+        return None
 
-    def visitSeq_of_statements(self, ctx: PlSqlParser.Seq_of_statementsContext):
-        ret = self.visitChildren(ctx)
+    def visitProcedure_body(self, ctx: PlSqlParser.Procedure_bodyContext):
+        visitor = Procedure_bodyVisitor(self.pkg_name)
+        ret = ctx.accept(visitor)
+        self.pkgs_calls_found += visitor.pkgs_calls_found
+        self.pkgs_calls_found = remove_duplicates(self.pkgs_calls_found)
         return ret
 
-    def visitDeclare_spec(self, ctx: PlSqlParser.Declare_specContext):
-        ret = self.visitChildren(ctx)
-        ret = full_flat_arr(ret)
-        name, value, *_ = ret + [None, None]
-        if value is None:
-            value = ast.NameConstant(value=None)
-        return ast.Assign(
-            targets=[name],
-            value=value
-        )
-
-    def visitStatement(self, ctx: PlSqlParser.StatementContext):
-        statements = self.visitChildren(ctx)
-        statement = statements[0]
-
-        return ast.Expr(
-            value=statement
-        )
-
-    def visitAssignment_statement(self, ctx: PlSqlParser.Assignment_statementContext):
-        ret = self.visitChildren(ctx)
-        name, value, *_ = full_flat_arr(ret) + [None, None]
-        return ast.Assign(
-            targets=[name],
-            value=value
-        )
-
-    def visitIf_statement(self, ctx: PlSqlParser.If_statementContext):
-        ret = self.visitChildren(ctx)
-        test = flat_arr(ret[0])[0]
-        body_expressions = flat_arr(ret[1])
-        return ast.If(
-            test=test,
-            body=body_expressions,
-            orelse=[]
-        )
-
-    def visitFunction_call(self, ctx: PlSqlParser.Function_callContext):
-        ret = self.visitChildren(ctx)
-        ret = full_flat_arr(ret)
-        ret = deque(ret)
-        routine_name = ret.popleft()
-        args = ret
-        return ast.Call(
-            func=routine_name,
-            args=args,
-            keywords=[]
-        )
-
-    def visitRoutine_name(self, ctx: PlSqlParser.Routine_nameContext):
-        ret = self.visitChildren(ctx)
-        pkg, method = flat_arr(ret)
-        if not pkg.id in self.pkgs_calls_found:
-            self.pkgs_calls_found.append(pkg.id)
-        return ast.Attribute(
-            value=pkg,
-            attr=method
-        )
-
-    def visitConcatenation(self, ctx: PlSqlParser.ConcatenationContext):
-        ret = self.visitChildren(ctx)
-        operands = full_flat_arr(ret)
-        if len(operands) == 2:
-            left, right = operands
-            operator = OPERATORS[ctx.op.text]()
-            return ast.BinOp(
-                left=left,
-                op=operator,
-                right=right
-            )
-        elif len(operands) == 1:
-            return operands
-        return ret
-
-    def visitRelational_expression(self, ctx: PlSqlParser.Relational_expressionContext):
-        ret = self.visitChildren(ctx)
-        expr = flat_arr(ret)
-        if len(expr) == 3:
-            left, operator, right = expr
-            return ast.Compare(
-                left=left,
-                ops=[operator],
-                comparators=[right]
-            )
-        return expr[0]
-
-    def visitRelational_operator(self, ctx: PlSqlParser.Relational_operatorContext):
-        text = ctx.getText()
-        operator = OPERATORS[text]
-        return operator()
-
-    def visitRegular_id(self, ctx: PlSqlParser.Regular_idContext):
-        the_id = ctx.REGULAR_ID().getText().upper()
-        return ast.Name(id=the_id)
-
-    def visitNull_statement(self, ctx: PlSqlParser.Null_statementContext):
-        return ast.Pass()
-
-    def visitNumeric(self, ctx: PlSqlParser.NumericContext):
-        num = int(ctx.getText())
-        return ast.Num(n=num)
-
-    def visitQuoted_string(self, ctx: PlSqlParser.Quoted_stringContext):
-        str_value = ctx.CHAR_STRING().getText()[1:-1]
-        return ast.Str(str_value)
-
-def full_flat_arr(arr):
-    return [elem for elem in find_elems(arr)]
-
-def find_elems(arr):
-    if arr is None:
-        return
-    if not isinstance(arr, list):
-        yield arr
-        return
-    for elem in arr:
-        yield from find_elems(elem)
-
-def flat_arr(arr):
-    return [find_life(elem) for elem in arr]
-
-def find_life(arr):
-    if isinstance(arr, list):
-        if len(arr) > 0:
-            return find_life(arr[0])
-        else:
-            return None
-    else:
-        return arr
+def get_spec_classname_by_classname(classname: str) -> str:
+    return "_" + classname + "_spec"
 
 class AntlrCaseInsensitiveFileInputStream(antlr4.FileStream):
 
@@ -271,7 +111,6 @@ class AntlrCaseInsensitiveFileInputStream(antlr4.FileStream):
             return antlr4.Token.EOF
         return self._lookaheadData[pos]
 
-
 def main(argv):
 
     if len(argv) == 2:
@@ -287,6 +126,7 @@ def main(argv):
     visitor = TheVisitor()
     node = tree.accept(visitor)
     #print(ast.dump(node))
+    #astpretty.pprint(node) # este modulo esta malo. no usar :(
     code = astor.to_source(node)
 
     output_filename = "built/generated/" + input_filename.split("/")[-1].split(".")[0] + ".py"
