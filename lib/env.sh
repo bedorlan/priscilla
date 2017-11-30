c97f8cf254ec9ab6d2a55e8d04972f97a5eef81d
bedorlan <bedorlan@gmail.com>
2017-11-30 12:41:00 -0500

    readd variables to env

--- lib/env.sh
+++ lib/env.sh
@@ -1,2 +1,6 @@
+root=$PWD
+export CLASSPATH=$root/antlr-4.7-complete.jar:$root/built
+export PYTHONPATH=$root/runtime_libs
+
 alias antlr4='java org.antlr.v4.Tool'
 alias grun='java org.antlr.v4.gui.TestRig'
