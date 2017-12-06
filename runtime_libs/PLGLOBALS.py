84cfbe84d5d5b03592a4eb15f67ef380a26514b8
bedorlan <orlando.bedoya@openintl.com>
2017-12-06 09:24:15 -0500

    instr

--- runtime_libs/PLGLOBALS.py
+++ runtime_libs/PLGLOBALS.py
@@ -1,3 +1,4 @@
+from PLHELPER import m, extract_value
 from PLCURSOR import PLCURSOR
 
 class _PL_EXCEPTION(RuntimeError):
@@ -6,8 +7,17 @@ class _PL_EXCEPTION(RuntimeError):
 class PLGLOBALS:
 
     @staticmethod
-    def MOD(n: int, mod: int) -> int:
-        return n % mod
+    def INSTR(st, sub):
+        st: str = extract_value(st)
+        sub: str = extract_value(sub)
+        index = st.find(sub)
+        return m(index + 1)
+
+    @staticmethod
+    def MOD(n, mod):
+        n = extract_value(n)
+        mod = extract_value(mod)
+        return m(n % mod)
 
     class LOGIN_DENIED(_PL_EXCEPTION):
         pass
