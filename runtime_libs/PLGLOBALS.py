ff06e03e62af91ec1f95f33e3aa78c0fc0cee553
bedorlan <orlando.bedoya@openintl.com>
2017-12-06 09:39:11 -0500

    replace

--- runtime_libs/PLGLOBALS.py
+++ runtime_libs/PLGLOBALS.py
@@ -1,4 +1,4 @@
-from PLHELPER import m, extract_value
+from PLHELPER import m, extract_value, ISNULL, NULL
 from PLCURSOR import PLCURSOR
 
 class _PL_EXCEPTION(RuntimeError):
@@ -18,6 +18,21 @@ class PLGLOBALS:
         n = extract_value(n)
         mod = extract_value(mod)
         return m(n % mod)
+
+    @staticmethod
+    def REPLACE(char, search, replacement=None):
+        if replacement is None:
+            replacement = NULL()
+        if ISNULL(char) or ISNULL(search):
+            return char
+        char: str = extract_value(char)
+        search: str = extract_value(search)
+        if not ISNULL(replacement):
+            replacement = extract_value(replacement)
+        else:
+            replacement = ""
+        value = char.replace(search, replacement)
+        return m(value)
 
     class LOGIN_DENIED(_PL_EXCEPTION):
         pass
